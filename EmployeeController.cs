09c69f6873f614ddc0296da4b893fa47b1d9cecd
prajyotnayak12 <110683704+prajyotnayak12@users.noreply.github.com>
2024-04-14 20:43:49 +0530

    Add files via upload

---
+++ EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeBusiness.Business;
+using EmployeeBusiness.Contract;
+using EmployeeModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeBusiness _EmployeeBusiness;
+        public EmployeeController(IEmployeeBusiness EmployeeBusiness)
+        {
+            _EmployeeBusiness = EmployeeBusiness;
+        }
+        [HttpGet()]
+        public IEnumerable<EmployeeMaster> Getemployee()
+        {
+            var result = _EmployeeBusiness.GetEmployee();
+            return result;
+        }
+    }
+
+    }
