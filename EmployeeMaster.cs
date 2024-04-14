87faa40163f6506d71f15af46fa226c83f0eb86a
prajyotnayak12 <110683704+prajyotnayak12@users.noreply.github.com>
2024-04-14 20:44:48 +0530

    Add files via upload

---
+++ EmployeeMaster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeModel
+{
+    public class EmployeeMaster
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public double Salary { get; set; }
+        public DateTime JoinDate { get; set; }
+    }
+}
